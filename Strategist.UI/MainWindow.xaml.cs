b123a0f44b45aeccc74fd02c9cb5e7c755235b79
AWhiteFox <ignat.k.150@gmail.com>
2020-10-30 11:22:06 +0300

    Initial commit

---
+++ Strategist.UI/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Strategist.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Matrix matrix = Matrix.Random(8, 8);
+        private int columnsSelected;
+        private Brush thresoldTextBoxDefaultBorderBrush;
+
+        public float? Threshold { get; set; } = 0.9f;
+        public DataTable MatrixAsDataTable { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            LoadDataTable();
+            GenerateCheckBoxes();
+        }
+
+        /// <summary>
+        /// Generates <see cref="CheckBox"/>es for enabling and didabling columns and rows
+        /// </summary>
+        private void GenerateCheckBoxes()
+        {
+            for (int i = 0; i < matrix.Columns; i++)
+            {
+                var cb = new CheckBox
+                {
+                    Content = matrix.ColumnHeaders[i],
+                    Tag = i,
+                    IsChecked = true
+                };
+                cb.Click += ColumnCheckBox_Click;
+                columnStackPanel.Children.Add(cb);
+            }
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                var cb = new CheckBox
+                {
+                    Content = matrix.RowHeaders[i],
+                    Tag = i,
+                    IsChecked = true
+                };
+                cb.Click += RowCheckBox_Click;
+                rowStackPanel.Children.Add(cb);
+            }
+        }
+
+        /// <summary>
+        /// Loads matrix into <see cref="MatrixAsDataTable"/>
+        /// </summary>
+        private void LoadDataTable()
+        {
+            MatrixAsDataTable = new DataTable();
+            MatrixAsDataTable.Columns.Add("Средство защиты", typeof(string));
+            for (int i = 0; i < matrix.Columns; i++)
+            {
+                MatrixAsDataTable.Columns.Add(matrix.ColumnHeaders[i], typeof(float));
+            }
+            for (int j = 0; j < matrix.Rows; j++)
+            {
+                LoadRow(j);
+            }
+            columnsSelected = matrix.Columns;
+        }
+
+        /// <summary>
+        /// Reads row with index <paramref name="rowIndex"/> from matrix and loads it into <see cref="MatrixAsDataTable"/>
+        /// </summary>
+        /// <param name="rowIndex"></param>
+        private void LoadRow(int rowIndex)
+        {
+            DataRow row = MatrixAsDataTable.NewRow();
+            row[0] = matrix.RowHeaders[rowIndex];
+            for (int i = 0; i < matrix.Columns; i++)
+            {
+                row[i + 1] = matrix.Values[i, rowIndex];
+            }
+            MatrixAsDataTable.Rows.Add(row);
+        }
+
+        /// <summary>
+        /// Called when this window is loaded
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid.Columns[0].Width = DataGridLength.Auto;
+            MatrixAsDataTable.DefaultView.Sort = MatrixAsDataTable.Columns[0].ColumnName;
+        }
+
+        /// <summary>
+        /// Shows or collapses columns when CheckBox is clicked
+        /// </summary>
+        private void ColumnCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var cb = (CheckBox)sender;
+            int i = (int)cb.Tag + 1;
+            if (cb.IsChecked.Value)
+            {
+                if (dataGrid.Columns[i].Visibility != Visibility.Visible)
+                {
+                    dataGrid.Columns[i].Visibility = Visibility.Visible;
+                    columnsSelected++;
+                }
+            }
+            else
+            {
+                if (dataGrid.Columns[i].Visibility == Visibility.Visible)
+                {
+                    dataGrid.Columns[i].Visibility = Visibility.Collapsed;
+                    columnsSelected--;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows or collapses rows when CheckBox is cliked
+        /// </summary>
+        private void RowCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var cb = (CheckBox)sender;
+            int i = (int)cb.Tag;
+
+            if (cb.IsChecked.Value)
+            {
+                LoadRow(i);
+            }
+            else
+            {
+                for (int j = 0; j < dataGrid.Items.Count; j++)
+                {
+                    if ((string)MatrixAsDataTable.Rows[j][0] == matrix.RowHeaders[i])
+                    {
+                        MatrixAsDataTable.Rows.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when Button is cliked
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Threshold is null)
+            {
+                MessageBox.Show("Неверный формат данных для минимальной вероятности защиты",
+                                "Ошибка",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                return;
+            }
+            if (columnsSelected == 0)
+            {
+                MessageBox.Show("Выберите хотя бы одну угрозу",
+                                "Ошибка",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                return;
+            }
+            if (MatrixAsDataTable.Rows.Count == 0)
+            {
+                MessageBox.Show("Выберите хотя бы одно средство защиты",
+                                "Ошибка",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                return;
+            }
+            MessageBox.Show("ok"); // TODO: Write actual code
+        }
+
+        /// <summary>
+        /// Checks if text in ThresholdTextBox can be pasrsed into float and if it is in the required range
+        /// </summary>
+        private void ThresholdTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // TODO: Consider creating deparate control for this
+            var tb = (TextBox)sender;
+            if (thresoldTextBoxDefaultBorderBrush is null)
+            {
+                thresoldTextBoxDefaultBorderBrush = tb.BorderBrush;
+            }
+            if (float.TryParse(tb.Text.Replace('.', ','), out float val) && 0f <= val && val <= 1f)
+            {
+                tb.BorderBrush = thresoldTextBoxDefaultBorderBrush;
+                Threshold = val;
+            }
+            else
+            {
+                tb.BorderBrush = Brushes.Red;
+                Threshold = null;
+            }
+        }
+    }
+}
---
+++ Strategist.UI/Matrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Strategist.UI  // TODO: Move to different project
+{
+    public class Matrix
+    {
+        public int Columns { get; }
+
+        public int Rows { get; }
+        
+        public string[] ColumnHeaders { get; }
+
+        public string[] RowHeaders { get; }
+
+        public float[,] Values { get; }
+
+        public Matrix(int ccount, int rcount)
+        {
+            Columns = ccount;
+            Rows = rcount;
+            ColumnHeaders = new string[ccount];
+            RowHeaders = new string[rcount];
+            Values = new float[ccount, rcount];
+        }
+
+        public static Matrix Random(int ccount, int rcount)
+        {
+            var m = new Matrix(ccount, rcount);
+            var rnd = new Random();
+
+            for (int i = 0; i < ccount; i++)
+            {
+                m.ColumnHeaders[i] = $"Угроза {i}";
+            }
+            for (int i = 0; i < rcount; i++)
+            {
+                m.RowHeaders[i] = $"Средство защиты {i}";
+            }
+
+            for (int j = 0; j < rcount; j++)
+            {
+                for (int i = 0; i < ccount; i++)
+                {
+                    m.Values[i, j] = (float)Math.Round(rnd.NextDouble(), 2);
+                }
+            }
+
+            return m;
+        }
+    }
+}
