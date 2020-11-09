0e6f5d137e2136445ccb4f2e936cf4eb0d223b3a
AWhiteFox <ignat.k.150@gmail.com>
2020-11-09 11:49:28 +0300

    Make ArrayExtensions more abstract -> IListExtensions

---
+++ Strategist.Core/Extensions/IListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategist.Core.Extensions
+{
+    public static class IListExtensions
+    {
+        public static void Fill<T>(this IList<T> list, Func<int, T> func)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = func.Invoke(i);
+            }
+        }
+
+        public static int FindIndex<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (predicate.Invoke(list[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
