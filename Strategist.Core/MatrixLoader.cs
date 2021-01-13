2a163db1e38c1b0d375e42042671bb305f3a067a
AWhiteFox <ignat.k.150@gmail.com>
2021-01-14 01:32:25 +0300

    Improve randomization

--- Strategist.Core/MatrixLoader.cs
+++ Strategist.Core/MatrixLoader.cs
@@ -1,5 +1,7 @@
 ﻿using Strategist.Core.Extensions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Strategist.Core
 {
@@ -12,25 +14,33 @@ namespace Strategist.Core
             int row_count = 3;
 
             var m = new Matrix();
+            var rnd = new Random();
 
             for (int i = 0; i < col_count; i++)
             {
                 m.AddColumn(new[] { $"Угроза {i}" });
             }
 
-            string[] rows = new string[row_count];
-            rows.Fill(i => $"Средство {i}");
-            foreach (var row in rows.Combinations())
+            var rows = new Dictionary<string, double[]>();
+            for (int i = 0; i < row_count; i++)
+            {
+                var arr = new double[col_count];
+                arr.Fill(_ => Math.Round(rnd.NextDouble(), 2));
+                rows[$"Средство {i}"] = arr;
+            }
+            
+            foreach (var row in rows.Keys.ToArray().Combinations())
             {
                 m.AddRow(row.ToArray());
-            }
-
-            var rnd = new Random();
-            for (int j = 0; j < m.Rows.Count; j++)
-            {
-                for (int i = 0; i < m.Columns.Count; i++)
+                int i = m.Rows.Count - 1;
+                for (int j = 0; j < col_count; j++)
                 {
-                    m[i, j] = Math.Round(rnd.NextDouble(), 2);
+                    double val = 0.0;
+                    foreach (var tag in row)
+                    {
+                        val = Math.Max(val, rows[tag][j]);
+                    }
+                    m[j, i] = val;
                 }
             }
 
