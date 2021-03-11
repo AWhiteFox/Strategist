e47d3f96e0e2e249abf0425568ce64ba58711a00
Dylariz <zarec-k@mail.ru>
2021-03-11 19:48:52 +0300

    Finish MongoDBMatrixLoader

--- Strategist.Loaders/MongoDBMatrixLoader.cs
+++ Strategist.Loaders/MongoDBMatrixLoader.cs
@@ -3,22 +3,22 @@ using Strategist.Core.Extensions;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
+using System.Linq;
 
 namespace Strategist.Core.MatrixLoaders
 {
-    public class MongoDBMatrixLoader
+    public class MongoDBMatrixLoader : MatrixLoader
     {
-        MongoClient client;
-        IMongoDatabase database;
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<StrategyModel> counterStrategiesCollection;
+        private IMongoCollection<StrategyModel> strategiesCollection;
+        private IMongoCollection<ProbabilityModel> probabilitiesCollection;
+        private readonly List<StrategyModel> counterStrategies = new List<StrategyModel>();
+        private readonly List<StrategyModel> strategies = new List<StrategyModel>();
+        private readonly List<ProbabilityModel> probabilities = new List<ProbabilityModel>();
+
         public Matrix MainMatrix { get; private set; }
-
-        IMongoCollection<StrategyModel> counterStrategiesCollection;
-        IMongoCollection<StrategyModel> strategiesCollection;
-        IMongoCollection<ProbabilityModel> probabilitiesCollection;
-
-        List<StrategyModel> counterStrategies = new List<StrategyModel>();
-        List<StrategyModel> strategies = new List<StrategyModel>();
-        List<ProbabilityModel> probabilities = new List<ProbabilityModel>();
 
         public MongoDBMatrixLoader(string connectionString, string databaseName)
         {
@@ -26,34 +26,9 @@ namespace Strategist.Core.MatrixLoaders
             GetDatabase(connectionString, databaseName);
         }
 
-        public Matrix GenerateMatrix()
+        public override Matrix Load()
         {
-            string[] namesStrategies = new string[strategies.Count];
-            string[] namesСounterStrategies = new string[counterStrategies.Count];
-
-            for (int i = 0; i < namesStrategies.Length; i++)
-                namesStrategies[i] = strategies[i].Name;
-
-            for (int i = 0; i < namesСounterStrategies.Length; i++)
-                namesСounterStrategies[i] = counterStrategies[i].Name;
-
-            foreach (var row in namesStrategies.Combinations())
-                MainMatrix.AddRow(row.ToArray());
-
-            foreach (var row in namesСounterStrategies.Combinations())
-                MainMatrix.AddColumn(row.ToArray());
-
-
-            /*for (int i = 0; i < probabilities.Count; i++)
-            {
-                for (int j = 0; j < probabilities[i].CounterStrategies.Length; j++)
-                {
-                    FindStrategyName(counterStrategies, probabilities[i].CounterStrategies[j]);
-
-                }
-            }*/
-
-            return MainMatrix;
+            return GenerateMatrix();
         }
 
         public void UpdateDocuments()
@@ -66,16 +41,56 @@ namespace Strategist.Core.MatrixLoaders
 
         public void GetDatabase(string connectionString, string databaseName)
         {
-            //Console.WriteLine("Подключение...");
             client = new MongoClient(connectionString);
             database = client.GetDatabase(databaseName);
-            //Console.WriteLine("Соединение установленно!\n\nПолучение коллекций...");
             counterStrategiesCollection = database.GetCollection<StrategyModel>("counter_strategies");
             strategiesCollection = database.GetCollection<StrategyModel>("strategies");
             probabilitiesCollection = database.GetCollection<ProbabilityModel>("probabilities");
-            //Console.WriteLine("Коллекции извлечены!\n");
 
             UpdateDocuments();
+        }
+
+        private Matrix GenerateMatrix()
+        {
+            var namesStrategies = new string[strategies.Count];
+            var namesСounterStrategies = new string[counterStrategies.Count];
+
+            for (int i = 0; i < namesStrategies.Length; i++)
+                namesStrategies[i] = strategies[i].Name;
+
+            for (int i = 0; i < namesСounterStrategies.Length; i++)
+                namesСounterStrategies[i] = counterStrategies[i].Name;
+
+            var matrixStrategies = namesStrategies.Combinations();
+            var matrixСounterStrategies = namesСounterStrategies.Combinations();
+
+            foreach (var row in matrixStrategies)
+                MainMatrix.AddRow(row);
+
+            foreach (var column in matrixСounterStrategies)
+                MainMatrix.AddColumn(column);
+
+            var fromProbabilitiesStrategies = new List<string>();
+            var fromProbabilitiesСounterStrategies = new List<string>();
+
+            for (int i = 0; i < probabilities.Count; i++)
+            {
+                foreach (var strategy in probabilities[i].Strategies)
+                    fromProbabilitiesStrategies.Add(FindStrategyName(strategies, strategy));
+
+                foreach (var strategy in probabilities[i].CounterStrategies)
+                    fromProbabilitiesСounterStrategies.Add(FindStrategyName(counterStrategies, strategy));
+
+                foreach (var row in matrixStrategies)
+                    foreach (var column in matrixСounterStrategies)
+                        if (new HashSet<string>(column).SetEquals(fromProbabilitiesСounterStrategies) && new HashSet<string>(row).SetEquals(fromProbabilitiesStrategies))
+                            MainMatrix[fromProbabilitiesСounterStrategies, fromProbabilitiesStrategies] = probabilities[i].Probability;
+
+                fromProbabilitiesStrategies.Clear();
+                fromProbabilitiesСounterStrategies.Clear();
+            }
+
+            return MainMatrix;
         }
 
         private string FindStrategyName(List<StrategyModel> strategies, ObjectId id)
@@ -90,7 +105,6 @@ namespace Strategist.Core.MatrixLoaders
 
         private void ExtractDocuments()
         {
-            //Console.WriteLine("Извлечение документов...");
             var strategiesFilter = Builders<StrategyModel>.Filter.Empty;
             var probabilitiesFilter = Builders<ProbabilityModel>.Filter.Empty;
             var docs = counterStrategiesCollection.Find(strategiesFilter).ToList();
@@ -108,26 +122,26 @@ namespace Strategist.Core.MatrixLoaders
             {
                 probabilities.Add(doc);
             }
-            //Console.WriteLine("Документы извлечены!");
         }
-    }
 
-    internal class StrategyModel
-    {
-        [BsonElement("_id")]
-        public ObjectId Id { get; set; }
-        [BsonElement("name")]
-        public string Name { get; set; }
-    }
-    internal class ProbabilityModel
-    {
-        [BsonElement("_id")]
-        public ObjectId Id { get; set; }
-        [BsonElement("strategies")]
-        public ObjectId[] Strategies { get; set; }
-        [BsonElement("counter_strategies")]
-        public ObjectId[] CounterStrategies { get; set; }
-        [BsonElement("probability")]
-        public double Probability { get; set; }
+        private class StrategyModel
+        {
+            [BsonElement("_id")]
+            public ObjectId Id { get; set; }
+            [BsonElement("name")]
+            public string Name { get; set; }
+        }
+
+        private class ProbabilityModel
+        {
+            [BsonElement("_id")]
+            public ObjectId Id { get; set; }
+            [BsonElement("strategies")]
+            public ObjectId[] Strategies { get; set; }
+            [BsonElement("counter_strategies")]
+            public ObjectId[] CounterStrategies { get; set; }
+            [BsonElement("probability")]
+            public double Probability { get; set; }
+        }
     }
 }
