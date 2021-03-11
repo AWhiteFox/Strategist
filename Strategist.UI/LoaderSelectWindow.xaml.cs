809cf4b01728d8563045cfb3c36684801b02bc3d
AWhiteFox <ignat.k.150@gmail.com>
2021-03-11 21:30:02 +0300

    Create LoaderSelectWindow markup

---
+++ Strategist.UI/LoaderSelectWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Strategist.UI
+{
+    public partial class LoaderSelectWindow : Window
+    {
+        public LoaderSelectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
