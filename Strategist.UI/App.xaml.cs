ca70929ee43def945e9ade762a984d3703c6d4cd
AWhiteFox <ignat.k.150@gmail.com>
2021-03-12 18:46:16 +0300

    Exit all threads on Application exit

--- Strategist.UI/App.xaml.cs
+++ Strategist.UI/App.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace Strategist.UI
+﻿using System;
+using System.Windows;
+
+namespace Strategist.UI
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
     public partial class App
     {
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
     }
 }
