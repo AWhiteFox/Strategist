8b2222058e349c8b45907d4062c488115b7e4d6e
AWhiteFox <ignat.k.150@gmail.com>
2021-04-19 20:56:44 +0300

    Finish JsonMatrixLoader.cs

--- Strategist.Loaders/JsonMatrixLoader.cs
+++ Strategist.Loaders/JsonMatrixLoader.cs
@@ -1,4 +1,9 @@
-﻿using Strategist.Core;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Strategist.Core;
+using Strategist.Core.Extensions;
 
 namespace Strategist.Loaders
 {
@@ -13,7 +18,49 @@ namespace Strategist.Loaders
         
         public override Matrix Load()
         {
-            throw new System.NotImplementedException();
+            var document = JsonConvert.DeserializeObject<Document>(File.ReadAllText(pathToFile));
+            if (document is null)
+                throw new ArgumentNullException(nameof(pathToFile));
+
+            var matrix = new Matrix();
+            
+            foreach (var strategies in document.Strategies.Combinations())
+                matrix.AddRow(strategies);
+            foreach (var counterStrategies in document.CounterStrategies.Combinations())
+                matrix.AddColumn(counterStrategies);
+
+            foreach (var test in document.Tests)
+            {
+                var rows = test.Strategies.Select(x => document.Strategies[x]);
+                var columns = test.CounterStrategies.Select(x => document.CounterStrategies[x]);
+                matrix[columns, rows] = test.SuccessProbability;
+            }
+
+            return matrix;
+        }
+
+        private class Document
+        {
+            [JsonProperty("strategies")]
+            public string[] Strategies { get; set; }
+            
+            [JsonProperty("counter_strategies")]
+            public string[] CounterStrategies { get; set; }
+            
+            [JsonProperty("tests")]
+            public StrategyTest[] Tests { get; set; }
+        }
+
+        private class StrategyTest
+        {
+            [JsonProperty("strategies")]
+            public int[] Strategies { get; set; }
+            
+            [JsonProperty("counter_strategies")]
+            public int[] CounterStrategies { get; set; }
+            
+            [JsonProperty("success_probability")]
+            public double SuccessProbability { get; set; }
         }
     }
 }
